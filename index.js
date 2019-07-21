8fa3dcf63bf53eae19aba7f363af8795a102e964
Heikki Uotinen <heikki.uotinen@elisanet.fi>
2019-07-21 10:43:25 +0300

    Data path

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('build'))
+
+app.use('/tre', function (req, res, next) {
+    console.log('Request Type:', req.method)
+    res.status(500).send('Just test!')
+    next()
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'))
