d328b76e90f280b3627a3754ef14c38399abb856
Heikki Uotinen <heikki.uotinen@elisanet.fi>
2019-07-21 12:48:10 +0300

    Fix

--- index.js
+++ index.js
@@ -6,22 +6,25 @@ const app = express()
 
 app.use(express.static('build'))
 
-https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-    let data = '';
+app.use('/tre', function (req, res, next) {
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+        let data = '';
 
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-        data += chunk;
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            //console.log(JSON.parse(data).explanation);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
     });
 
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-        //console.log(JSON.parse(data).explanation);
-    });
-
-}).on("error", (err) => {
-    console.log("Error: " + err.message);
-});
+})
 
 /*
 app.use('/tre', function (req, res, next) {
