23af64293f7e0c54ffe8ce3abb44a20a3313015d
Heikki Uotinen <heikki.uotinen@elisanet.fi>
2019-07-21 12:55:55 +0300

    Test

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
 const express = require('express');
-const https = require('https');
-//const axios = require('axios');
+const axios = require('axios');
 
 const app = express()
 
 app.use(express.static('build'))
 
+// https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+
+//axios.get('https://context.tampere.fiware.cityvision.cloud/v2/entities?limit=800&offset=0&type=Streetlight%2CStreetlightControlCabinet%2CAmbientLightSensor%2CWasteContainer',
+// { 'headers': { "FIWARE-Service": "tampere" } })
+
+
 app.use('/tre', function (req, res, next) {
-    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            //console.log(JSON.parse(data).explanation);
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
-
-    next();
-})
-
-/*
-app.use('/tre', function (req, res, next) {
-    axios.get('https://context.tampere.fiware.cityvision.cloud/v2/entities?limit=800&offset=0&type=Streetlight%2CStreetlightControlCabinet%2CAmbientLightSensor%2CWasteContainer',
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
             { 'headers': { "FIWARE-Service": "tampere" } })
         .then(response => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/plain');
-            res.end(response.data);
+            //res.end(response.data);
+            res.end('ok');
             console.log('ok:', response.data);            
         })
         .catch(error => {
@@ -46,6 +30,6 @@ app.use('/tre', function (req, res, next) {
     //next()
     //res.end('completed');
 })
-*/
+
 
 app.listen(3000, () => console.log('Server running on port 3000'))
