8cbda67325eee09b6c63c30e8047bd1b4948f31a
Heikki Uotinen <heikki.uotinen@elisanet.fi>
2019-07-21 11:03:57 +0300

    Log

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.use(express.static('build'))
 app.use('/tre', function (req, res, next) {
     axios.get('https://context.tampere.fiware.cityvision.cloud/v2/entities?limit=800&offset=0&type=Streetlight%2CStreetlightControlCabinet%2CAmbientLightSensor%2CWasteContainer')
         .then(response => {
-            console.log('ok, ', response.data);
+            res.send(response.data);
+            console.log('ok, ', response.data);            
         })
         .catch(error => {
             res.status(500).send('[HT0001] Error in get data : '+error);
@@ -17,6 +18,7 @@ app.use('/tre', function (req, res, next) {
     //console.log('Request Type:', req.method)
 
     //next()
+    res.end('completed');
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
