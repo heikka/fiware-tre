32e7ac0f373a1bd7a262c76443d5e99399c31053
Heikki Uotinen <heikki.uotinen@elisanet.fi>
2019-07-21 12:40:33 +0300

    Std

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require('express')
-const axios = require('axios');
+const express = require('express');
+const https = require('https');
+//const axios = require('axios');
 
 const app = express()
 
 app.use(express.static('build'))
 
+https.get('https://context.tampere.fiware.cityvision.cloud/v2/entities?limit=800&offset=0&type=Streetlight%2CStreetlightControlCabinet%2CAmbientLightSensor%2CWasteContainer', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+/*
 app.use('/tre', function (req, res, next) {
     axios.get('https://context.tampere.fiware.cityvision.cloud/v2/entities?limit=800&offset=0&type=Streetlight%2CStreetlightControlCabinet%2CAmbientLightSensor%2CWasteContainer',
             { 'headers': { "FIWARE-Service": "tampere" } })
         .then(response => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
             res.end(response.data);
             console.log('ok:', response.data);            
         })
@@ -21,5 +42,6 @@ app.use('/tre', function (req, res, next) {
     //next()
     //res.end('completed');
 })
+*/
 
 app.listen(3000, () => console.log('Server running on port 3000'))
